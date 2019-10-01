ca0e19372ed5bafb9f6be14faa8c4737593e3f3e
specspecialist <43399263+specspecialist@users.noreply.github.com>
2019-10-01 13:18:03 +0530

    Update mediafire_com.php

--- hosts/download/mediafire_com.php
+++ hosts/download/mediafire_com.php
@@ -6,111 +6,102 @@ if (!defined('RAPIDLEECH')) {
 }
 
 class mediafire_com extends DownloadClass {
+	private $page = '', $cookie = array(), $fid, $link;
 	public function Download($link) {
-		$Cookies = array();
 		if (!empty($_POST['mfpassword'])) {
-			$Cookies = StrToCookies(urldecode($_POST['cookie']));
-			$page = $this->GetPage($link, $Cookies, array('downloadp' => urlencode($_POST['mfpassword'])), $link);
+			$this->cookie = StrToCookies(urldecode($_POST['cookie']));
+			$this->page = $this->GetPage($link, $this->cookie, array('downloadp' => urlencode($_POST['mfpassword'])), $link);
+			$this->cookie = GetCookiesArr($this->page, $this->cookie);
 		}
 
-		$link = preg_replace('@https?://([^/]+\.)?mediafire\.com/(?!download/)((download\.php)|(file/)|(view/))?\??@i', 'http://www.mediafire.com/download/', $link);
-		$page = $this->GetPage($link, $Cookies);
-		$Cookies = GetCookiesArr($page, $Cookies);
-		if (preg_match('@\nLocation: .*(/download/([^/\r\n]+)/?[^\r\n]*)@i', $page, $redir)) {
-			$link = 'http://www.mediafire.com'.$redir[1];
-			$page = $this->GetPage($link, $Cookies);
-			$Cookies = GetCookiesArr($page, $Cookies);
-		}
-		if (preg_match('@/error\.php\?errno=\d+@i', $page, $redir)) {
-			$page = $this->GetPage('http://www.mediafire.com'.$redir[0]);
-			if (preg_match('@error_msg_title">\s*([^\r\n<>]+)\s*<@i', $page, $err)) html_error($err[1]);
-			html_error('Link is not available');
-		}
+		if (!preg_match('@https?://(?:[\w\-]+\.)*mediafire\.com/(?:(download|file)/|(download\.php|file/|view/)?\??)([\w\-\.]+)(?(1)(/[^/\s]+))?@i', $link, $this->fid)) html_error('Invalid Link?');
 
-		$this->MF_Captcha($link, $page);
-		if (strpos($page, 'name="downloadp" id="downloadp"')) {
-			$DefaultParam = $this->DefaultParamArr(preg_replace('@/download/([^/]+)/?.*@i', '/?$1', $link), $Cookies);
-			$html = '<form action="index.php" method="POST">';
-			foreach ($DefaultParam as $key => $value) $html.='<input type="hidden" name="' . $key . '" value="' . $value . '"/>';
-			$html.='Enter your password here </br><input type="text" name="mfpassword" value="" placeholder="Enter your password here" autofocus="autofocus" required="required" /><input type="submit" name="action" value="Submit"/></form>';
-			echo $html;
-			exit;
-		}
-		if (preg_match('@Location: (http:\/\/[^\r\n]+)@i', $page, $dl) || preg_match('@\w+\s*=\s*\"(https?://[^\"]+)\"\s*;@i', $page, $dl)) {
-			$dlink = trim($dl[1]);
-			$this->RedirectDownload($dlink, "Mediafire.com", $Cookies);
-		} else html_error("Error: Download link [FREE] not found!");
-	}
+		$this->link = $GLOBALS['Referer'] = 'http://www.mediafire.com/file/' . $this->fid[3] . (!empty($this->fid[4]) ? $this->fid[4] : '');
 
-	private function MF_Captcha($link, &$page) {
-		if (stripos($page, ">Authorize Download</a>") === false) return;
-		if (!empty($_POST['step']) && $_POST['step'] == 1) {
-			if (empty($_POST['recaptcha_response_field']) && empty($_POST['captcha'])) html_error('You didn\'t enter the image verification code.');
-			if (empty($_POST['captcha'])) $post = array('recaptcha_challenge_field' => $_POST['recaptcha_challenge_field'], 'recaptcha_response_field' => $_POST['recaptcha_response_field']);
-			else {
-				$post = array();
-				foreach ($_POST['T8'] as $n => $v) $post[urlencode($n)] = $v;
-				$post['adcopy_response'] = $_POST['captcha'];
-
-				$url = 'http://api.solvemedia.com/papi/verify.noscript';
-				$page = $this->GetPage($url, 0, $post, $link);
-
-				if (!preg_match('@(https?://[^/\'\"<>\r\n]+)?/papi/verify\.pass\.noscript\?[^/\'\"<>\r\n]+@i', $page, $resp)) {
-					is_present($page, '/papi/challenge.noscript', 'Wrong CAPTCHA entered.');
-					html_error('Error sending CAPTCHA.');
-				}
-				$resp = (empty($resp[1])) ? 'http://api.solvemedia.com'.$resp[0] : $resp[0];
-
-				$page = $this->GetPage($resp, 0, 0, $url);
-				if (!preg_match('@>[\s\t\r\n]*([^<>\r\n]+)[\s\t\r\n]*</textarea>@i', $page, $gibberish)) html_error('CAPTCHA response not found.');
-
-				$post = array('adcopy_challenge' => urlencode($gibberish[1]), 'adcopy_response' => 'manual_challenge');
+		if (empty($_POST['step']) || $_POST['step'] != '1') {
+			$this->page = $this->GetPage($this->link, $this->cookie, 0, 'http://www.mediafire.com/?' . $this->fid[3]);
+			$this->cookie = GetCookiesArr($this->page, $this->cookie);
+			if (preg_match('@\nLocation: .*(/file/([^/\r\n]+)/?[^\r\n]*)@i', $this->page, $redir)) {
+				$this->link = $GLOBALS['Referer'] = 'http://www.mediafire.com'.$redir[1];
+				$this->page = $this->GetPage($this->link, $this->cookie);
+				$this->cookie = GetCookiesArr($this->page, $this->cookie);
 			}
-			$page = $this->GetPage(str_ireplace('mediafire.com/download/', 'mediafire.com/?', $link), 0, $post);
-			is_present($page, 'Your entry was incorrect, please try again!');
-			$page = $this->GetPage($link, 0, $post);
+			if (preg_match('@/error\.php\?errno=\d+@i', $this->page, $redir)) {
+				$this->page = $this->GetPage('http://www.mediafire.com'.$redir[0]);
+				if (preg_match('@error_msg_title(?: notranslate)?">\s*([^\r\n<>]+)\s*<@i', $this->page, $err)) html_error($err[1]);
+				html_error('Link is not available');
+			}
+		}
+
+		$this->MF_Captcha();
+		if (strpos($this->page, 'name="downloadp" id="downloadp"')) {
+			$DefaultParam = $this->DefaultParamArr(preg_replace('@/file/([^/]+)/?.*@i', '/?$1', $link), $this->cookie);
+			echo "<form action='".htmlspecialchars($GLOBALS['PHP_SELF'], ENT_QUOTES)."' method='POST'>\n";
+			foreach ($DefaultParam as $key => $value) echo "<input type='hidden' name='$key' value='" . htmlspecialchars($value, ENT_QUOTES) . "' />\n";
+			echo "Enter your password here:<br />\n<input type='text' name='mfpassword' value='' placeholder='Enter file password here' autofocus='autofocus' required='required' />\n<input type='submit' />\n</form>";
+			return html_error('File requires password');
+		}
+		if (!preg_match('@https?://(?:[\w\-]+\.)+[\w\-]+(?:\:\d+)?/[\w\-\.]{5,}/' . preg_quote($this->fid[3]) . '/[^\?\'\"\t<>\r\n\\\]+@i', $this->page, $dl)) return html_error("Error: Download link [FREE] not found!");
+		$this->RedirectDownload($dl[0], 'Mediafire.com');
+	}
+
+	private function MF_Captcha() {
+		if (!empty($this->page) && stripos($this->page, ">Authorize Download</a>") === false) return;
+		if (!empty($_POST['step']) && $_POST['step'] == '1') {
+			$_POST['step'] = false;
+			if (empty($_POST['recaptcha2_response_field']) && empty($_POST['recaptcha_response_field']) && empty($_POST['adcopy_response']) && empty($_POST['mf_captcha_response'])) html_error('You didn\'t enter the image verification code.');
+			if (empty($_POST['mf_captcha_response'])) {
+				if (empty($_POST['adcopy_response'])) {
+					if (empty($_POST['recaptcha2_public_key'])) $post = array('recaptcha_challenge_field' => urlencode($_POST['recaptcha_challenge_field']), 'recaptcha_response_field' => urlencode($_POST['recaptcha_response_field']));
+					else $post = $this->verifyReCaptchav2();
+				} else $post = $this->verifySolveMedia();
+				$this->cookie = StrToCookies(urldecode($_POST['cookie']));
+			} else {
+				$post = array('mf_captcha_response' => $_POST['mf_captcha_response']);
+			}
+
+			$purl = 'http://www.mediafire.com/?' . $this->fid[3];
+
+			$this->page = $this->GetPage($purl, $this->cookie, $post);
+			$this->cookie = GetCookiesArr($this->page, $this->cookie);
+			is_present($this->page, 'Your entry was incorrect, please try again!');
+
+			$this->page = $this->GetPage($this->link, $this->cookie, 0, $purl);
+			$this->cookie = GetCookiesArr($this->page, $this->cookie);
+			is_present($this->page, 'Your entry was incorrect, please try again!.');
+
+			$this->MF_Captcha();
 		} else {
-			if (!preg_match('@https?://(?:[^/]+\.)?(?:(?:google\.com/recaptcha/api)|(?:recaptcha\.net))/(?:(?:challenge)|(?:noscript))\?k=([\w|\-]+)@i', $page, $pid) && !preg_match('@http://api\.solvemedia\.com/papi/challenge\.noscript\?k=[\w\.-]+@i', $page, $spid)) html_error('Error: CAPTCHA not found.');
-			$data = $this->DefaultParamArr($link);
+			$data = $this->DefaultParamArr($this->link, $this->cookie);
 			$data['step'] = 1;
-			if (!empty($spid)) {
-				$page = $this->GetPage($spid[0], 0, 0, $link);
-				if (!preg_match('@<img [^/<>]*src\s?=\s?\"((https?://[^/\"<>]+)?/papi/media[^\"<>]+)\"@i', $page, $imgurl)) html_error('CAPTCHA img not found.');
-				$imgurl = (empty($imgurl[2])) ? 'http://api.solvemedia.com'.$imgurl[1] : $imgurl[1];
 
-				if (!preg_match_all('@<input [^/|<|>]*type\s?=\s?\"?hidden\"?[^/<>]*\s?name\s?=\s?\"(\w+)\"[^/<>]*\s?value\s?=\s?\"([^\"<>]+)\"[^/<>]*/?\s*>@i', $page, $forms)) html_error('CAPTCHA data not found.');
-				$forms = array_combine($forms[1], $forms[2]);
-				foreach ($forms as $n => $v) $data["T8[$n]"] = urlencode($v);
+			if (($pos = stripos($this->page, 'data-sitekey=')) !== false && preg_match('@data-sitekey=\s*[\"\']([\w\.\-]+)[\"\']@i', $this->page, $cKey, 0, $pos)) {
+				// reCAPTCHA v2
+				return $this->reCAPTCHAv2($cKey[1], $data);
+			} else if (($pos = stripos($this->page, '://api.solvemedia.com/')) !== false && preg_match('@https?://api\.solvemedia\.com/papi/challenge\.(?:no)?script\?k=([\w\.\-]+)@i', $this->page, $cKey, 0, $pos)) {
+				// SolveMedia
+				return $this->SolveMedia($cKey[1], $data);
+			} else if (preg_match('@https?://(?:[^/]+\.)?(?:(?:google\.com/recaptcha/api)|(?:recaptcha\.net))/(?:(?:challenge)|(?:noscript))\?k=([\w\.\-]+)@i', $this->page, $cKey)) {
+				// Old reCAPTCHA
+				return $this->reCAPTCHA($cKey[1], $data);
+			} else if (($pseudoCaptcha = cut_str($this->page, 'name="mf_captcha_response" value="', '"'))) {
+				// Best... CAPTCHA... Ever... :D
+				if (!empty($_POST['mf_captcha_response'])) html_error('Captcha Loop?');
+				$_POST['step'] = '1';
+				$_POST['mf_captcha_response'] = html_entity_decode($pseudoCaptcha);
+				return $this->MF_Captcha();
+			}
 
-				//Download captcha img.
-				$page = $this->GetPage($imgurl);
-				$capt_img = substr($page, strpos($page, "\r\n\r\n") + 4);
-				$imgfile = DOWNLOAD_DIR . 'mediafire_captcha.gif';
-
-				if (file_exists($imgfile)) unlink($imgfile);
-				if (!write_file($imgfile, $capt_img)) html_error('Error getting CAPTCHA image.');
-
-				$this->EnterCaptcha($imgfile.'?'.time(), $data, 20);
-				exit();
-			} else $this->Show_reCaptcha($pid[1], $data);
+			html_error('Error: CAPTCHA not found.');
 		}
 	}
 
-	private function Show_reCaptcha($pid, $inputs, $sname = 'Download File') {
-		global $PHP_SELF;
-		if (!is_array($inputs)) html_error('Error parsing captcha data.');
+	// Special Function Called by verifyReCaptchav2 When Captcha Is Incorrect, To Allow Retry. - Required
+	protected function retryReCaptchav2() {
+		$data = $this->DefaultParamArr($this->link, $this->cookie);
+		$data['step'] = '1';
 
-		// Themes: 'red', 'white', 'blackglass', 'clean'
-		echo "<script language='JavaScript'>var RecaptchaOptions = {theme:'red', lang:'en'};</script>\n";
-
-		echo "\n<center><form name='recaptcha' action='$PHP_SELF' method='post'><br />\n";
-		foreach ($inputs as $name => $input) echo "<input type='hidden' name='$name' id='$name' value='$input' />\n";
-		echo "<script type='text/javascript' src='http://www.google.com/recaptcha/api/challenge?k=$pid'></script>";
-		echo "<noscript><iframe src='http://www.google.com/recaptcha/api/noscript?k=$pid' height='300' width='500' frameborder='0'></iframe><br /><textarea name='recaptcha_challenge_field' rows='3' cols='40'></textarea><input type='hidden' name='recaptcha_response_field' value='manual_challenge' /></noscript><br />";
-		echo "<input type='submit' name='submit' onclick='javascript:return checkc();' value='$sname' />\n";
-		echo "<script type='text/javascript'>/*<![CDATA[*/\nfunction checkc(){\nvar capt=document.getElementById('recaptcha_response_field');\nif (capt.value == '') { window.alert('You didn\'t enter the image verification code.'); return false; }\nelse { return true; }\n}\n/*]]>*/</script>\n";
-		echo "</form></center>\n</body>\n</html>";
-		exit;
+		return $this->reCAPTCHAv2($_POST['recaptcha2_public_key'], $data);
 	}
 }
 
@@ -127,6 +118,9 @@ class mediafire_com extends DownloadClass {
  * quick fix for new format/redirect for share links && fixed capcha submit url by Th3-822 24-05-2013
  * fixed password post url by Th3-822 27-07-2013
  * fixed redirects by Th3-822 24-09-2013
+ * fixed captcha forms by Th3-822 11-04-2014
+ * added recaptcha v2 captcha support && fixed dead link msgs by Th3-822 07-04-2015
+ * added checkbox "captcha" support (non tested) by Th3-822 18-06-2015
+ * fixed link regexp, redirect by Th3-822 18-10-2016
+ * fixed download regexp by Th3-822 09-05-2018
  */
- 
-?>
