5e496d00542e3de65d919a6182488c22c4a49e6f
specspecialist <43399263+specspecialist@users.noreply.github.com>
2019-10-01 12:40:33 +0530

    Update rl_init.php

--- rl_init.php
+++ rl_init.php
@@ -7,16 +7,18 @@ if (count(get_included_files()) == 1) {
 }
 
 @set_time_limit(0);
-ini_alter('memory_limit', '1024M');
+@ini_set('memory_limit', '1024M');
+if (ini_get('zlib.output_compression')) @ini_set('zlib.output_compression', 0);
 if (ob_get_level()) ob_end_clean();
 ob_implicit_flush(true);
+header('X-Accel-Buffering: no');
 clearstatcache();
 error_reporting(6135);
 $nn = "\r\n";
 $fromaddr = 'RapidLeech';
 $dev_name = 'Development Stage';
 $rev_num = '43';
-$plusrar_v = '4.1';
+$plusrar_v = '4.2';
 $PHP_SELF = $_SERVER['SCRIPT_NAME'];
 define('RAPIDLEECH', 'yes');
 define('ROOT_DIR', realpath('./'));
@@ -25,7 +27,7 @@ define('HOST_DIR', 'hosts/');
 define('CLASS_DIR', 'classes/');
 define('CONFIG_DIR', 'configs/');
 define('BUILD', '30May2011');
-define('CREDITS', '<a href="http://www.rapidleech.com/" class="rl-link"><b>RapidLeech</b></a>&nbsp;<b class="rev-dev">PlugMod (eqbal) rev. ' . $rev_num . '</b> <span class="rev-dev">' . $dev_name . '</span><br><small class="small-credits">Credits to Pramode &amp; Checkmate &amp; Kloon</small><br /><p class="rapidleechhost"><a href="http://www.rapidleechhost.com/aff.php?aff=001" target="_blank">RapidleechHost Offical Hosting</a></p>');
+define('CREDITS', '<a href="http://www.rapidleech.com/" class="rl-link"><b>RapidLeech</b></a>&nbsp;<b class="rev-dev">PlugMod (eqbal) rev. ' . $rev_num . '</b> <span class="rev-dev">' . $dev_name . '</span><br><small class="small-credits">Credits to Pramode &amp; Checkmate &amp; Kloon</small>');
 
 require_once(CONFIG_DIR . 'setup.php');
 
@@ -35,12 +37,12 @@ define('DOWNLOAD_DIR', (substr($options['download_dir'], 0, 6) == 'ftp://' ? '' 
 define('TEMPLATE_DIR', 'templates/' . $options['template_used'] . '/');
 define('IMAGE_DIR', TEMPLATE_DIR . 'images/');
 header('X-Frame-Options: SAMEORIGIN');
-if ($options['no_cache']) {
-	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-	header('Last-Modified: ' . gmdate ("D, d M Y H:i:s") . 'GMT');
-	header('Cache-Control: no-cache, must-revalidate, max-age=0');
-	header('Pragma: no-cache');
-}
+// Avoid Caching
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate ("D, d M Y H:i:s") . 'GMT');
+header('Cache-Control: max-age=0, no-store, no-cache, must-revalidate, proxy-revalidate, post-check=0, pre-check=0');
+header('Pragma: no-cache');
+
 require_once(CLASS_DIR . 'other.php');
 
 
